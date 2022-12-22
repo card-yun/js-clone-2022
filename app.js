a5ccc36ed2fcec7b105a22bce35c5b7211c650f5
Ca-rd <91603898+Ca-rd@users.noreply.github.com>
2022-12-23 03:14:09 +0900

    js Base3 - return value

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
-//personName이라는 거는 sayHello라는 function의 body 안에서만 사용 가능하다.
-function sayHello(personName){
-    console.log(personName);
-};
-
-sayHello("nico");
-
-//player를 object라고 하고, sayHello는 function이자 property라고 한다.
-//const abc = 10 라고 했을때, abc를 variable이라고 한다.
-const player={
-    sayHello: function(nameOfPerson){
-        console.log("Hello " + nameOfPerson +" Nice to meet you!")
-    }
-}
-//"nicolas"를 argument 라고 한다.//
-player.sayHello("nicolas");
-
-//예제//
-//순서 중요!
-
-const calculator = {
+//return하는 순간 function은 종료된다.
+const calculator={
     plus: function(a, b){
-        console.log(a+b);
+        return a+b
     },
     minus: function(a, b){
-        console.log(a-b);
+        return a-b
     },
-    divide: function(a, b){
-        console.log(a/b);
-    },
-    powerOf: function(a, b){
-        console.log(a**b);
-    }
 }
 
-calculator.plus(2,5);
-calculator.minus(2,5);
-calculator.divide(2,5);
-calculator.powerOf(2,5);
+//variable인 plusResult는 function의 return값을 가지게 된다.
+//return을 정의하는 것.
+const plusResult = calculator.plus(2, 3);
+
+//예제2
+function calculateKrAge(foreignerAge){
+    foreignerAge+2;
+    return "Hello";
+}
+
+const krAge = calculateKrAge(24);
+
+console.log(krAge);
