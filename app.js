0e7b36ab7fe8f5cbd2266f858c22fe6c8a368fa2
Ca-rd <91603898+Ca-rd@users.noreply.github.com>
2022-12-24 01:31:29 +0900

    querySelector - js document

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const age = parseInt(prompt("How old are you?"));
+/*js가 html을 가져올 수 있다.
+app.js를 import했기 때문에 document(검색창)가 존재할 수 있다.
+--내 html과 app.js가 연결 됨--
+browser가 우리가 document 에 접근할 수 있게 해준다.*/
+//ID랑 Class로 가져오기: getElementById/ClassNmae
+const innerBox = document.getElementById("inputBox");
+console.dir(innerBox);
 
-if(isNaN(age) || age < 0){
-    console.log("Please write a real positive number.");
-} else if(age<18){
-    console.log("You are too young...");
-} else if(age>=18 && age<=50){
-    console.log("You can drink!")
-} else if(age>50 && age<=65){
-    console.log("You should exercise...");
-} else if(age===100){
-    console.log("WOW you are wise.")
-}  else if(age>65){
-    console.log("You can do everything, hahaha!");
-} 
+const borderBox = document.getElementById("submitBox");
+console.dir(borderBox);
+
+//CSS처럼 Selector
+const title = document.querySelector(".hello h1:first-child");
+console.log(title);
+
+title.style.color="blue";
+
+const test = document.querySelector("#abcd");
+console.log(test);
